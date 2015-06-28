ea8454da14f8bc4ab88498a071db39b6684df09f
mjp2220 <mjp2220@columbia.edu>
2015-06-28 02:25:37 -0700

    Adding rotten tomatoes

--- SpeakEasyApp/ViewController.swift
+++ SpeakEasyApp/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController, UIWebViewDelegate, WitDelegate {
   let wit: Wit = Wit.sharedInstance()
   var isRecording: Bool = false
   var isLoaded: Bool = true
+  var numberOfWebViews: Int = 0
+  var webActions: [String] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    webView.loadRequest(NSURLRequest(URL: NSURL(string: "http://news.ycombinator.com")!))
+    webView.delegate = self
+    webView.loadRequest(NSURLRequest(URL: NSURL(string: "http://wikipedia.com")!))
     addMicButton()
     wit.delegate = self
     wit.setContext(["state" : "news.ycombinator.com"])
@@ -39,29 +42,37 @@ class ViewController: UIViewController, UIWebViewDelegate, WitDelegate {
   // MARK: UIWebViewDelegate
   func webViewDidFinishLoad(webView: UIWebView) {
     isLoaded = true
+    webView.stringByEvaluatingJavaScriptFromString("document.getElementById('searchInput').value = 'Matt'")
+    numberOfWebViews--
+    println("Number of views: \(numberOfWebViews)")
+    if numberOfWebViews == 0 {
+      println("Web Actions: \(webActions)")
+      if webActions.count > 0 {
+        let currentAction = webActions[0]
+        webView.stringByEvaluatingJavaScriptFromString(currentAction)
+        webActions.removeAtIndex(0)
+        println("Web Actions: \(webActions)")
+      }
+    }
   }
   
   func webViewDidStartLoad(webView: UIWebView) {
     isLoaded = false
+    numberOfWebViews++
+    println("Number of views: \(numberOfWebViews)")
+  }
+
+  func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+    numberOfWebViews--
+    println("Number of views: \(numberOfWebViews)")
   }
   
   // MARK: WitDelegate
   func witDidGraspIntent(outcomes: [AnyObject]!, messageId: String!, customData: AnyObject!, error e: NSError!) {
     if outcomes != nil {
       let outcome: NSDictionary = outcomes[0] as! NSDictionary
-      let response: NSString = outcome.valueForKeyPath("intent") as! NSString
-      let sampleResponse: NSString = deserialize(response as String)
-      println(sampleResponse)
-      let resultData: NSData = sampleResponse.dataUsingEncoding(NSUTF8StringEncoding)!
-      var result: NSDictionary = NSJSONSerialization.JSONObjectWithData(resultData, options: nil, error: nil) as! NSDictionary
-      let selectors = result.valueForKeyPath("data")! as! NSArray
-      for index in 0...selectors.count-1 {
-        let selector = selectors[index] as! NSDictionary
-        let selectorName: String = selector.valueForKeyPath("data.selector")! as! String
-        println(selectorName)
-        while !isLoaded {}
-        handleResponse(selectorName)
-      }
+      println(outcome)
+      handleOutcome(outcome)
     } else {
       println("BALLS")
     }
@@ -83,6 +94,23 @@ class ViewController: UIViewController, UIWebViewDelegate, WitDelegate {
   
   func startRecord() {
     wit.start()
+  }
+
+  // MARK: Link Handling
+  func handleOutcome(outcome: NSDictionary) {
+    let intent: NSString = outcome.valueForKeyPath("intent") as! NSString
+    if intent == "google" {
+      let search_query: NSString = outcome.valueForKeyPath("entities.search_query.value")![0] as! NSString
+      let newURL: String = "https://www.google.com/search?q=\(search_query)"
+      webView.loadRequest(NSURLRequest(URL: NSURL(string: newURL.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!))
+    } else if intent == "rottentomatoes" {
+      let search_query: NSString = outcome.valueForKeyPath("entities.search_query.value")![0] as! NSString
+      let newURL: String = "http://www.rottentomatoes.com/search/?search=\(search_query)"
+      webView.loadRequest(NSURLRequest(URL: NSURL(string: newURL.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!))
+      let clickResult = "document.querySelectorAll('#movie_results_ul a')[0].click()"
+      webActions.append(clickResult)
+    }
+    let search_query: NSString = outcome.valueForKeyPath("entities.search_query.value")![0] as! NSString
   }
 }
 
